606daa33c3827519a1651a0d3223ae23a9b4f83b
Raju Gharti <gharti.rg@gmail.com>
2023-12-09 16:43:07 -0500

    initial setup

---
+++ src/main/java/com/spring_boot_web_api_basics/SpringBootWebApiBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_web_api_basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebApiBasicsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebApiBasicsApplication.class, args);
+    }
+
+}
